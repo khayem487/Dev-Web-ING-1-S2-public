725db42e78cfa616806a02268ce01ced3174c165
Anastasia <anastasiatsybulskaya0@gmail.com>
2026-04-14 10:49:00 +0200

    premier commit : initialisation du projet

---
+++ backend/src/main/java/com/projdevweb/MaisonConnectee.java
@@ -0,0 +1,12 @@
+package com.projdevweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisonConnectee {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaisonConnectee.class, args);
+    }
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '40px'}}>
+            <h1> OK Frontend React opérationnel </h1>
+            <p>prêt à se connecter au backend Spring Boot (API)</p>
+        </div>
+    )
+}
